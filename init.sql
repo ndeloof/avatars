66599d28d9adfc3ea81236c55e32ecbd0d250226
Nicolas De Loof <nicolas.deloof@gmail.com>
2023-03-31 15:22:15 +0200

    use config and run to inject initial data
    Signed-off-by: Nicolas De Loof <nicolas.deloof@gmail.com>

---
+++ init.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS clothing;
+CREATE TABLE clothing (company VARCHAR(255) PRIMARY KEY, color VARCHAR(255));
+INSERT INTO clothing (company, color) VALUES ("tilt", "#20BA31");
+INSERT INTO clothing (company, color) VALUES ("docker", "#086DD7");
